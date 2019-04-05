b020eeab346b028e76fbe823e1c78f656051de62
Kyle Baron <kyleb@metrumrg.com>
2019-04-05 10:41:52 -0500

    re-add content

---
+++ content/inst/script/build.R
@@ -0,0 +1,6 @@
+library(rmarkdown)
+render("tools_optimization_intro.Rmd", output_format = "github_document")
+render("tools_optimization_intro.Rmd", output_format = "html_document")
+
+
+
---
+++ content/inst/script/global.R
@@ -0,0 +1,10 @@
+#.libPaths("/data/Rlibs")
+options(mrgsolve.soloc = "build")
+knitr::opts_chunk$set(
+  comment = '.', 
+  fig.height = 5, 
+  fig.width = 9, 
+  fig.align = "center",
+  message = FALSE
+)
+ggplot2::theme_set(ggplot2::theme_bw())
