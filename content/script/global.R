905cd36b8269d2c4a1a3940e462cd5359f6c815d
Kyle Baron <kyleb@metrumrg.com>
2019-04-09 09:58:33 -0500

    rebuild

--- content/script/global.R
+++ content/script/global.R
@@ -3,8 +3,6 @@ options(mrgsolve.soloc = "build")
 knitr::opts_chunk$set(
   comment = '.', 
   fig.align = "center",
-  message = FALSE, 
-  fig.width = 5, 
-  fig.height = 4
+  message = FALSE
 )
 ggplot2::theme_set(ggplot2::theme_bw())
